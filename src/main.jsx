639d00675d6c702f791f824a00609fa8ebe27d6c
Rohit singh <rohitdev124421@gmail.com>
2025-03-20 12:07:35 +0530

    Updated feature XYZ or fixed issue ABC

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+ import { Provider } from "react-redux";
 import store from "./Redux/store"; // Import the store
 import App from "./App";
 
